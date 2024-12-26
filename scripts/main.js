05cf7ab78e0b6ea6fe6f5cf770035fc025f57f1c
rshaker24 <rtshaker24@gmail.com>
2024-12-26 01:35:19 -0500

    fixed gameOver modal

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,12 @@
-const words = ["HANGMAN", "COLLEGE", "JAVASCRIPT", "CODING", "PROJECT",];
 const keyboardDiv = document.querySelector(".keyboard");
 const gameEndWindow = document.querySelector(".game-end-window");
 const playAgainButton = document.querySelector(".play-again-button");
 const responseMessage = document.querySelector(".js-response-message")
 const strikesMessage = document.querySelector(".js-strikes-message")
+const hangmanImage = document.querySelector(".hangman-image")
 
-gameEndWindow.style.visibility = 'hidden';      // Hide
+gameEndWindow.style.visibility = 'hidden';
+responseMessage.innerHTML = "Welcome to Hangman!";      // Hide
 generateWord()
 
 //event listener
@@ -18,39 +19,43 @@ console.log(wordArray);
 //create underscored version of wordChoice
 let wordStatus = "_".repeat(wordChoice.length);
 let wordStatusArray = wordStatus.split('')
-console.log(wordStatusArray)
+
 
 //create strike counter
 let strikes = 0;
-
-//create guessedLetters array
-let guessedLetters = [];
 
 //generate random word
 function generateWord(){
     genNum = Math.floor(Math.random() * words.length - 0);
     wordChoice = words[genNum];
     console.log(wordChoice);
+    wordArray = wordChoice.split('');
+}
+
+function updateImage(){
+
 }
 
 //reset the game state
 function resetGame(){
-    console.log("The game has been reset!")
     strikes = 0;
     updateStrikes();
+    responseMessage.innerHTML = "Welcome to Hangman!";
     keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
     gameEndWindow.style.visibility = 'hidden';
     generateWord();
     wordStatus = "_".repeat(wordChoice.length); //repeated from before - fix
     wordStatusArray = wordStatus.split('')
     renderWord(wordStatusArray);
+    hangmanImage.src = `images/hangman-image.png`;
+    console.log(wordStatusArray);
 }
 
 // refreshes the word display - changing underscores to letters
 function renderWord(wordStatusArray){
     let wordHTML = ''
     for(i = 0; i < wordChoice.length; i++){
-        console.log(wordStatusArray[i])
+        //console.log(wordStatusArray[i])
         if(wordStatusArray[i] === '_'){
             const html = `
                 <li>_</li>
@@ -58,14 +63,14 @@ function renderWord(wordStatusArray){
             wordHTML += html;
         }
         else{
-            console.log('adding letter: ',wordArray[i])
+            //console.log('adding letter: ',wordArray[i])
             const html = `
                 <li>${wordArray[i]}</li>
             `
             wordHTML += html;
         }
     }
-    console.log(wordHTML)
+    //console.log(wordHTML)
 
     document.querySelector('.js-word-container')
         .innerHTML = wordHTML
@@ -91,66 +96,39 @@ function initGame(button, clickedLetter){
             updateWordStatus(wordArray, wordStatusArray, guess);
             
         }else{
-            console.log("The word does not contain ", guess);
+            //console.log("The word does not contain ", guess);
             strikes++;
             updateStrikes();
+            hangmanImage.src = `images/hangman-image-${strikes}.png`;
             responseMessage.innerHTML = "Incorrect Guess"
             //setTimeout(() => {document.querySelector(".js-response-message").innerHTML = ""}, 3000);     
         }   
     }
-    else{
-        gameEndWindow.style.visibility = 'visible';      // Hide
-
-    } 
-}
-
-
-      
-
-
-
-//Updates Guessed Letters box
-/*
-function updateGuessedLetters(guess){
-    let guessHTML =''
-    if(!guessedLetters.includes(guess)){
-        guessedLetters.push(guess);
-        const html = `
-            <li>${guessedLetters}</li>
-        `
-        guessHTML += html;
-    }
-
-    document.querySelector(".js-guessed-letters-display")
-        .innerHTML = guessHTML;
+    if(!wordStatusArray.includes("_")) return gameOver(true);
+    console.log(wordStatusArray);
 
 }
-*/
 
 //Updates the strikes counter
 function updateStrikes(){
     //strikes++;
     //document.querySelector('.js-strikes-message').innerHTML = "Incorrect Guesses: " + strikes + "/6";
+    strikesMessage.innerHTML = "Incorrect Guesses: " + strikes + "/6";
     if (strikes === 6){
-        strikesMessage.innerHTML = "You Lose!";
-        gameEndWindow.style.visibility = 'visible';
-    }
-    else{
-        strikesMessage.innerHTML = "Incorrect Guesses: " + strikes + "/6";
+        return gameOver(false) 
     }
 
 }
 
 // Updates wordStatusArray with new letter
 function updateWordStatus(wordArray, wordStatusArray, guess){
-    console.log('Ran updateWordStatus functon!');
+    //console.log('Ran updateWordStatus functon!');
     for(let i = 0; i < wordArray.length; i++)
     {
         if (wordArray[i] === guess) {
-            console.log('replacing: ', wordStatusArray[i], 'with: ', wordArray[i])
+            //console.log('replacing: ', wordStatusArray[i], 'with: ', wordArray[i])
             wordStatusArray[i] = wordArray[i]; // Replace underscore with guessed letter
-            console.log('new word status array: ',wordStatusArray);
-            console.log(wordArray);
+            //console.log('new word status array: ',wordStatusArray);
             renderWord(wordStatusArray);
         }
     }
@@ -160,14 +138,22 @@ function updateWordStatus(wordArray, wordStatusArray, guess){
 //Creating Keyboard Layout - WIP
 for (let i = 97; i <= 122; i++) {
     const button = document.createElement("button");
-    button.innerText = String.fromCharCode(i);
+    button.innerText = String.fromCharCode(i).toUpperCase();
     keyboardDiv.appendChild(button);
     button.addEventListener("click", (e) => initGame(e.target, String.fromCharCode(i)));
 }
 
 //
 
-function checkWin(){
+function gameOver(isVictory){
+    // After game complete.. showing modal with relevant details
+    console.log(isVictory);
+    gameEndWindow.style.visibility = 'visible';
+    const modalText = isVictory ? `You found the word:` : 'The correct word was:';
+    gameEndWindow.querySelector("img").src = `images/${isVictory ? 'victory' : 'lose'}.gif`;
+    gameEndWindow.querySelector("h4").innerText = isVictory ? 'Congrats!' :  'Game Over!'
+    gameEndWindow.querySelector("p").innerHTML = `${modalText} <b>${wordChoice}</b>`;
 
 }
+
 
---
+++ scripts/words.js
@@ -0,0 +1,8 @@
+const words = ["HANGMAN", "COLLEGE", "JAVASCRIPT", "CODING", "PROJECT",];
+
+const WordList = [
+    {
+        word: "HANGMAN",
+        description: "A popular word game"
+    },
+]
